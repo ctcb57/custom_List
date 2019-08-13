1e505b793e41f309fa1a54b00f89e39764a63f70
Charles Clark <ctcb57@gmail.com>
2019-08-13 14:08:30 -0500

    Unit test methods for Add method created

---
+++ custom_List/custom_List/CustomList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace custom_List
+{
+    //Steps for the project:
+    //Make sure all of the unit tests are created
+    //Create an indexer within the class
+    //Generate the logic within the add method 
+    //Logic Steps:
+    //need to have item[0] = item that is passed as a parameter
+    //this needs to be a for loop though where it is item[i] and predicated on the capacity of the array
+    //when the capacity has been reached, there needs to be logic which will double the size of the array
+    //for this, it will reinstantiate an array which is the same size as the previous array size
+    //need to combine these two arrays while also keeping the numbers at the correct index locations
+    //Test the logic when it is complete to ensure the method works
+    //move onto the remove method
+    public class CustomList<T>
+    {
+        //member variables
+        private T[] customList;
+        private T count;
+        private T capacity;
+
+        public T Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+
+        //constructor
+
+
+        //member methods
+        public void Add(T itemToAdd)
+        {
+
+        }
+    }
+}
---
+++ custom_List/custom_List_Test/custom_List_Test.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using custom_List;
+
+namespace custom_List_Test
+{
+    [TestClass]
+    public class custom_List_Test
+    {
+        [TestMethod]
+        public void Add_AddToEmptyList_ItemGoesToIndexZero()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(1);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddToListWithExistingItem_NewItemGoesToEndOfList()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            // act
+            testList.Add(1);
+            testList.Add(2);
+            actual = testList[1];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddToEmptyList_CountGoesToOne()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(198765);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_AddToFullList_AddsToEndOfList()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 5;
+            int actual;
+
+            //act
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            actual = testList[4];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //create a test to see if it is doubled twice
+
+        public void Add_AddToFullListSecondIteration_AddsToEndOfList()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int expected = 9;
+            int actual;
+            //act
+            testList.Add(9);
+            actual = testList[9];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
